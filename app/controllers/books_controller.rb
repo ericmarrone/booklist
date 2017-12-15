2adf5278630f1339ff7fca428beda49d4ca5ec08
Eric Marrone <emarrone17@gmail.com>
2017-12-15 16:41:07 -0500

    Add respont_to block

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,23 @@
 class BooksController < ApplicationController
+  def index
+    @books = Book.all
 
+    respond_to do |format|
+      format.json do
+        render json: { books: @books }
+      end
 
+      format.html do
+        render :index
+      end
+
+      format.text do
+        render :index
+      end
+
+      format.csv do
+        render plain: Book.generate_csv(@books)
+      end
+    end
+  end
 end
